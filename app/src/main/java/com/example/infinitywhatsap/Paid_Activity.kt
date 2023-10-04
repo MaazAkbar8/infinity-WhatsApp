1599718b0af3c541f055a41a043621c19ed227a1
Dell <https://github.com/MaazAkbar8/Die-Roll-Dise.git>
2023-10-04 12:58:18 -0700

    initial comment

---
+++ app/src/main/java/com/example/infinitywhatsap/Paid_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.infinitywhatsap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Paid_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_paid)
+    }
+}
---
+++ app/src/main/java/com/example/infinitywhatsap/categoryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.infinitywhatsap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class categoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_category)
+    }
+}
